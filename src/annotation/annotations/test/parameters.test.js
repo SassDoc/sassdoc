65bb25a169abd2972f0ed92e79421ea2c5c9291c
Fabrice Weinberg <Fabrice@weinberg.me>
2014-07-07 22:00:24 +0200

    update

--- src/annotation/annotations/test/parameters.test.js
+++ src/annotation/annotations/test/parameters.test.js
@@ -8,10 +8,14 @@ describe('#parameters', function(){
   var param  = require('../parameters.js');
 
   it('should return an object', function(){
-    // needs fix-param
-    // assert.deepEqual(param('{type} $hyphenated-name (default) - description'), { type : 'type', name :'hyphenated-name', default : 'default', description : 'description'});
-    // assert.deepEqual(param('{type} $name (()) - description'), { type : 'type', name :'name', default : '()', description : 'description'});
+    assert.deepEqual(param('{type} $hyphenated-name (default) - description'), { type : 'type', name :'hyphenated-name', default : 'default', description : 'description'});
+    assert.deepEqual(param('{type} $name (()) - description'), { type : 'type', name :'name', default : '()', description : 'description'});
     assert.deepEqual(param('{List} $list - list to check'), { type : 'List', name :'list', description : 'list to check'});
+  });
+
+  it('should parse all chars in type', function(){
+    assert.deepEqual(param('{*} $name - description'), { type : '*', name :'name', description : 'description'});
+    assert.deepEqual(param('{type|other} $name - description'), { type : 'type|other', name :'name', description : 'description'});
   });
 
 });
