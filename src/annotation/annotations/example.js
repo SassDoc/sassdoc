981b36402b5c10f4416e1ce797cb4329376ad40d
Fabrice Weinberg <Fabrice@weinberg.me>
2014-07-07 20:10:14 +0200

    there could be spaces after example annotation

--- src/annotation/annotations/example.js
+++ src/annotation/annotations/example.js
@@ -16,7 +16,7 @@ module.exports = function (text) {
   // Get the optional type info
   var optionalType = text.substr(0, text.indexOf('\n'));
 
-  if (optionalType.length !== 0) {
+  if (optionalType.trim().length !== 0) {
     example.type = optionalType;
     example.code = text.substr(optionalType.length + 1); // Remove the type
   }
