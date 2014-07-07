4c52ccd010d8e2a927d7fc76f690365e4350dcb4
Fabrice Weinberg <Fabrice@weinberg.me>
2014-07-07 19:49:15 +0200

    Add type info to example

--- src/annotation/annotations/example.js
+++ src/annotation/annotations/example.js
@@ -1,5 +1,27 @@
 'use strict';
 
+/**
+ * @example is a multiline annotation
+ * check if there is something on the first line and use it as the type information
+ *
+ * @example html
+ * <div></div>
+ */
 module.exports = function (text) {
-  return text;
+  var example = {
+    type : 'scss', // Default to scss
+    code : text
+  };
+
+  // Get the optional type info
+  var optionalType = text.substr(0, text.indexOf('\n'));
+
+  if (optionalType.length !== 0) {
+    example.type = optionalType;
+    example.code = text.substr(optionalType.length + 1); // Remove the type
+  }
+
+  // remove all leading/trailing line breaks.
+  example.code = example.code.replace(/^\n|\n$/g, '');
+  return example;
 };
