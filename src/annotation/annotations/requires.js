fbc2ce541af6afcca7f56183be5d84d11b9da258
Fabrice Weinberg <Fabrice@weinberg.me>
2014-07-09 16:48:58 +0200

    Fix #74

--- src/annotation/annotations/requires.js
+++ src/annotation/annotations/requires.js
@@ -3,8 +3,18 @@
 var reqRegEx = /\s*(?:\{([\w-_]+)\}\s*)?(.*)/;
 module.exports = function (text) {
   var match = reqRegEx.exec(text);
+
+  var type = match[1] || 'function';
+  var name = match[2];
+
+  if (name.indexOf('$') === 0){
+    type = 'variable';
+    name = name.slice(1);
+  }
+
+
   return {
-    type : match[1] || 'function',
-    name : match[2]
+    type : type,
+    name : name
   };
 };
--- src/annotation/annotations/test/requires.test.js
+++ src/annotation/annotations/test/requires.test.js
@@ -8,6 +8,16 @@ describe('#requires', function(){
   it('should default to function', function(){
     assert.deepEqual(requires('name'), { type : 'function', name : 'name'});
   });
+
+  it('should work for variables with with or without $', function(){
+    assert.deepEqual(requires('{variable} $my-variable'), { type : 'variable', name : 'my-variable'});
+    assert.deepEqual(requires('{variable} my-variable'),  { type : 'variable', name : 'my-variable'});
+  });
+
+  it('should work with optional variable type if $ is used', function(){
+    assert.deepEqual(requires('{variable} my-variable'), { type : 'variable', name : 'my-variable'});
+    assert.deepEqual(requires('$my-variable'), { type : 'variable', name : 'my-variable'});
+  });
 });
 
 
