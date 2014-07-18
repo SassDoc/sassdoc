6d0522ba73bd440e06484164680360db9e71f531
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-07-18 16:22:27 +0200

    Added an extra test for link annotation

--- src/annotation/annotations/test/link.test.js
+++ src/annotation/annotations/test/link.test.js
@@ -12,6 +12,7 @@ describe('#link', function(){
   it('should return the caption optionally', function(){
     assert.deepEqual(link('http://sass.com'), { url : 'http://sass.com', caption : ''});
     assert.deepEqual(link('http://sass.com caption'), { url : 'http://sass.com', caption : 'caption'});
+    assert.deepEqual(link('http://sass.com multiple words caption'), { url : 'http://sass.com', caption : 'multiple words caption'});
   });
 });
 
