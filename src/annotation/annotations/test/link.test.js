8a0b1e4ea8f4ec018a96bcc0b4cda11294340940
Fabrice Weinberg <Fabrice@weinberg.me>
2014-07-18 17:37:05 +0200

    Fix `@list` annotation

--- src/annotation/annotations/test/link.test.js
+++ src/annotation/annotations/test/link.test.js
@@ -9,6 +9,10 @@ describe('#link', function(){
     assert.deepEqual(link(''), { url : '', caption : ''});
   });
 
+  it('should work with funny spaces and linebreaks', function(){
+    assert.deepEqual(link('\t\n\nhttp://sass.com    \t\n\n'), { url : 'http://sass.com', caption : ''});
+  });
+
   it('should return the caption optionally', function(){
     assert.deepEqual(link('http://sass.com'), { url : 'http://sass.com', caption : ''});
     assert.deepEqual(link('http://sass.com caption'), { url : 'http://sass.com', caption : 'caption'});
