84fd4c7447e10a705f8d8df57ad5c5fc6d7afbd6
Fabrice Weinberg <Fabrice@weinberg.me>
2014-07-26 15:56:07 +0200

    Update parse interface

--- src/parser.js
+++ src/parser.js
@@ -3,4 +3,25 @@
 var ScssCommentParser = require('scss-comment-parser');
 var annotations = require('./annotation');
 
-module.exports = new ScssCommentParser(annotations);
+var parser = new ScssCommentParser(annotations);
+
+module.exports = {
+
+  parse : parser.parse.bind(parser),
+
+  /**
+   * Invoke the `resolve` function of an annotation if present
+   * Called with all found annotations excluding them from "unkown" type
+   */
+  postProcess : function(data){
+
+    Object.keys(annotations).forEach(function (key){
+      var annotation = annotations[key];
+      if (annotation.resolve){
+        annotation.resolve(data);
+      }
+    });
+
+    return data;
+  }
+};
