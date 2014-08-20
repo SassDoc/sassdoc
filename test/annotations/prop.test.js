1a6002c530ed21c627fa89011d6a198fd2bb1a6d
Fabrice Weinberg <Fabrice@weinberg.me>
2014-08-20 21:27:40 +0200

    Implement #25

---
+++ test/annotations/prop.test.js
@@ -0,0 +1,27 @@
+/* global describe, it */
+'use strict';
+
+var assert = require('assert');
+
+describe('#prop', function () {
+  var prop = require('../../src/annotation').prop;
+
+  it('should parse the prop annotation', function () {
+    assert.deepEqual(prop.parse('base'), {
+      type : 'Map',
+      path : 'base'
+    });
+
+    assert.deepEqual(prop.parse('{Function} base.default - description'), {
+      type : 'Function',
+      path : 'base.default',
+      description : 'description'
+    });
+
+    assert.deepEqual(prop.parse('{Function} base.default - description'), {
+      type : 'Function',
+      path : 'base.default',
+      description : 'description'
+    });
+  });
+});
