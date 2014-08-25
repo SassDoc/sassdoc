94464de0b51939b44ae63e155f0c8bd076ba5869
Valérian Galliat <val@codejam.info>
2014-08-25 17:22:13 +0200

    Better test for `requires` annotation
    Testing circular references and JSON representation.

--- test/data/requires.test.js
+++ test/data/requires.test.js
@@ -25,12 +25,28 @@ describe('#requires', function () {
      * @see https://twitter.com/HugoGiraudel/status/503253144940601344
      */
     var dataClone = _.clone(data, true);
+    var expectedClone = _.clone(expected , true);
 
-    delete dataClone['function']['test-function-requires'].requires.toJSON;
-    assert.deepEqual(dataClone, expected);
+    var requires = dataClone['function']['test-function-requires'];
+    var required = dataClone['function']['test-function-required'];
+
+    assert.equal(requires.requires[0].item, required);
+    assert.equal(required.usedBy[0], requires);
+
+    // Remove circular structure from the clone
+    delete requires.requires.toJSON;
+    delete requires.requires[0].item;
+    delete required.usedBy.toJSON;
+
+    // Include the reference
+    expectedClone['function']['test-function-required'].usedBy[0] = expectedClone['function']['test-function-requires'];
+
+    assert.deepEqual(dataClone, expectedClone);
   });
 
   it('should be JSON serializable', function () {
+    var dataClone = _.clone(data, true);
+
     assert.equal(JSON.stringify(data, null, '  '), JSON.stringify(expected, null, '  '));
   });
 });
