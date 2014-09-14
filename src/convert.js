f9c91a7ad190e44d37ba1c6c9cf2ef5d77a8029c
Pascal Duez <pascal@duez.me>
2014-09-14 14:22:58 +0200

    Check for `sass-convert` binary prior to run the convert task

--- src/convert.js
+++ src/convert.js
@@ -2,13 +2,14 @@
 
 var path = require('path');
 var cp = require('child_process');
-var rimraf = require('rimraf');
 var Q = require('q');
 
-var rmdir = Q.denodeify(rimraf);
+var which = Q.denodeify(require('which'));
+var rmdir = Q.denodeify(require('rimraf'));
 
 /**
  * Execute a child process command.
+ *
  * @see    {@link http://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback}
  * @param  {String} command
  * @param  {Object} options
@@ -43,50 +44,102 @@ function exec(command) {
   return deferred.promise;
 }
 
+/**
+ * SassDoc converter constructor.
+ */
+function Converter(api) {
+  this.tmpDir = path.join(process.cwd(), '.tmp');
+  this.src = '';
+  this.useBundler = false;
+  this.api = api;
+}
+
+/**
+ * Check whether passed binary (Gem) is in $PATH.
+ *
+ * @param  {String} bin
+ * @return {Q.Promise}
+ */
+Converter.prototype.checkBinary = function (bin) {
+  var self = this;
+
+  return which(bin)
+    .then(null, function () {
+      return which('bundle')
+        .then(function () {
+          self.useBundler = true;
+          // `bundle show bin` was more adapted but
+          // `sassdoc-convert` is not a gem.
+          var command = 'bundle exec ' + bin + ' -v';
+          return exec(command);
+        })
+    })
+    .catch(function (err) {
+      err.message = 'SassDoc could not find any executable for "' +
+                    bin + '". Operation Aborted.'
+      throw err;
+    });
+};
+
+/**
+ * Format the `sass-convert command string.
+ *
+ * @param  {Boolean} bundler
+ * @return {String}
+ */
+Converter.prototype.convertCommand = function () {
+  var command = [
+    'sass-convert',
+    '-R',
+    '--from=sass',
+    '--to=scss',
+    this.src,
+    this.tmpDir
+  ];
+
+  if (this.useBundler) {
+    command.unshift('bundle exec');
+  }
+
+  return command.join(' ');
+}
+
+/**
+ * Perform a sass to scss syntax convertion
+ * and run SassDoc against the resulting files.
+ *
+ * @param  {String} src
+ * @param  {String} dest
+ * @param  {Object} config
+ * @return {Q.Promise}
+ */
+Converter.prototype.documentize = function (src, dest, config) {
+  var self = this;
+  var sassdoc = self.api;
+  self.src = src;
+
+  return self.checkBinary('sass-convert')
+    .then(function () {
+      rmdir(self.tmpDir);
+    })
+    .then(function () {
+      sassdoc.logger.log('Converting sass into scss syntax.');
+      return exec(self.convertCommand());
+    })
+    .then(function () {
+      return sassdoc.documentize(self.tmpDir, dest, config);
+    })
+    .then(function () {
+      return rmdir(self.tmpDir);
+    })
+    .catch(function (err) {
+      sassdoc.logger.enabled = true;
+      sassdoc.logger.error(err);
+    });
+}
+
 module.exports = function (sassdoc) {
   sassdoc = sassdoc || require('./api');
 
-  /**
-   * Perform a sass to scss convertion and run SassDoc against the result.
-   * @param  {String} src
-   * @param  {String} dest
-   * @param  {Object} config
-   * @return {Q.Promise}
-   */
-  function documentize(src, dest, config) {
-    var tmpDir = path.join(process.cwd(), '.tmp');
-    var useBundler = false;
-    var command = [
-      'sass-convert',
-      '-R',
-      '--from=sass',
-      '--to=scss',
-      src,
-      tmpDir
-    ];
-
-    if (useBundler) {
-      command.unshift('bundle exec');
-    }
-
-    command = command.join(' ');
-
-    return rmdir(tmpDir)
-      .then(function () {
-        return exec(command);
-      })
-      .then(function () {
-        return sassdoc.documentize(tmpDir, dest, config);
-      })
-      .then(function () {
-        return rmdir(tmpDir);
-      })
-      .catch(function (err) {
-        console.error('ERROR: ', err);
-      });
-  }
-
-  return {
-    documentize: documentize
-  };
+  return new Converter(sassdoc);
 };
