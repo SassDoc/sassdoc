51be5fe9c91eb27d51bf5a86049697f28591885e
Pascal Duez <pascal@duez.me>
2014-09-20 13:08:55 +0200

    test: update and enable `sass-convert` option tests

--- test/api/convert.test.js
+++ test/api/convert.test.js
@@ -7,9 +7,11 @@ var fs = require('fs');
 var mkdirp = require('mkdirp');
 var rimraf = require('rimraf');
 
+
 describe('#sass-convert', function () {
   var converter = require('../../src/convert')();
   var src = path.join(__dirname, 'fixture/convert');
+  var file = path.join(converter.tmpDir, 'test.scss');
   converter.src = src;
 
   describe('Sass to SCSS convertion', function () {
@@ -19,7 +21,13 @@ describe('#sass-convert', function () {
 
     it('should produce SCSS files', function () {
       assert.ok(fs.existsSync(converter.tmpDir));
-      assert.ok(fs.existsSync(path.join(converter.tmpDir, 'test.scss')));
+      assert.ok(fs.existsSync(file));
+    });
+
+    it('should contain correct comments', function () {
+      var regex = /\/\*\*/;
+      var body = fs.readFileSync(file, 'utf8');
+      assert.ok(regex.test(body), file + ' did not match \'' + regex + '\'.');
     });
   });
 
@@ -38,6 +46,13 @@ describe('#sass-convert', function () {
       assert.ok(fs.existsSync('tmp/docs'));
       assert.ok(fs.existsSync('tmp/docs/index.html'));
       assert.ok(fs.existsSync('tmp/docs/assets'));
+    });
+
+    it('should contain correct content', function () {
+      var regex = /test-mix-public/;
+      var index = 'tmp/docs/index.html';
+      var body = fs.readFileSync(index, 'utf8');
+      assert.ok(regex.test(body), index + ' did not match \'' + regex + '\'.');
     });
 
     it('should clean the temporary dir', function () {
