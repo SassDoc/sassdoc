e6f29f45b640adcff8d68f1d231edec9aad42521
Fabrice Weinberg <Fabrice@weinberg.me>
2014-10-05 18:14:47 +0200

    Implement throws autofill

--- src/annotation/annotations/throws.js
+++ src/annotation/annotations/throws.js
@@ -1,8 +1,20 @@
 'use strict';
 
+var autoParserError = /@error\s+(?:'|")([^'"]+)/g;
+
 module.exports = {
   parse: function (text) {
     return text.trim();
   },
-  alias: ['throw', 'exception']
+  default : function(item){
+    var match;
+    var throwing = [];
+    while ( (match = autoParserError.exec(item.context.code)) ) {
+      throwing.push(match[1]);
+    }
+    // Workaround till `default` API is updated!
+    item['throws'] = throwing;
+  },
+  alias: ['throw', 'exception'],
+  allowedOn: ['function', 'mixin', 'placeholder']
 };
