598db3aaa16f3f6de17fb53ef99390c03f29d2fb
Fabrice Weinberg <Fabrice@weinberg.me>
2014-10-13 18:56:24 +0200

    fix alias and content

--- src/annotation/annotations/alias.js
+++ src/annotation/annotations/alias.js
@@ -12,26 +12,25 @@ module.exports = {
   resolve: function (byTypeAndName) {
     utils.eachItem(byTypeAndName, function (item) {
       if (utils.isset(item.alias)) {
-        item.alias.forEach(function (alias) {
+        var type = item.context.type;
+        var name = item.context.name;
 
-          var type = item.context.type;
-          var name = item.context.name;
+        if (utils.isset(byTypeAndName[type]) &&
+            utils.isset(byTypeAndName[type][alias])) {
 
-          if (utils.isset(byTypeAndName[type]) &&
-              utils.isset(byTypeAndName[type][alias])) {
-
-            if (!Array.isArray(byTypeAndName[type][alias].aliased)) {
-              byTypeAndName[type][alias].aliased = [];
-            }
-
-            byTypeAndName[type][alias].aliased.push(name);
-
+          if (!Array.isArray(byTypeAndName[type][alias].aliased)) {
+            byTypeAndName[type][alias].aliased = [];
           }
-          else {
-            logger.log('Item `' + name + '` is an alias of `' + alias + '` but this item doesn\'t exist.');
-          }
-        });
+
+          byTypeAndName[type][alias].aliased.push(name);
+
+        }
+        else {
+          logger.log('Item `' + name + '` is an alias of `' + alias + '` but this item doesn\'t exist.');
+        }
       }
     });
-  }
+  },
+
+  multiple : false
 };
