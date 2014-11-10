198e0cd658114464c5e966fbaba5de9474144487
Valérian Galliat <val@codejam.info>
2014-11-10 22:52:29 +0100

    ES6, please!

---
+++ src/sassdoc.js
@@ -0,0 +1,23 @@
+export function resolveConfig(path=null) {
+
+}
+
+export function refresh(dest) {
+
+}
+
+export function read(src) {
+
+}
+
+export function parse() {
+
+}
+
+export function process(ctx) {
+
+}
+
+export function theme(dest, ctx) {
+
+}
