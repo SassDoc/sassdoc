66257c480fd4bcc20e6475ca17044e7ffee2cf7a
Valérian Galliat <val@codejam.info>
2014-11-16 13:23:47 +0100

    Port sorter to ES6

--- src/sorter.js
+++ src/sorter.js
@@ -1,39 +1,31 @@
-'use strict';
+export default data => {
+  var sorted = {};
+
+  Object.keys(data).sort().forEach(function (type) {
+    sorted[type] = {};
+
+    Object.keys(data[type])
+      .map(function (name) {
+        return data[type][name];
+      })
+      .sort(function (a, b) {
+        return compare(a.group[0][0].toLowerCase(), b.group[0][0].toLowerCase()) ||
+               compare(a.file.path, b.file.path) ||
+               compare(a.context.line.start, b.context.line.start);
+
+      })
+      .forEach(function (item) {
+        sorted[type][item.context.name] = item;
+      });
+  });
+
+  return sorted;
+};
 
 function compare(a, b) {
-  if (a > b) {
-    return 1;
+  switch (true) {
+    case a > b: return 1;
+    case a === b: return 0;
+    default: return -1;
   }
-
-  if (a === b) {
-    return 0;
-  }
-
-  return -1;
 }
-
-module.exports = {
-  postProcess: function (data) {
-    var sorted = {};
-
-    Object.keys(data).sort().forEach(function (type) {
-      sorted[type] = {};
-
-      Object.keys(data[type])
-        .map(function (name) {
-          return data[type][name];
-        })
-        .sort(function (a, b) {
-          return compare(a.group[0][0].toLowerCase(), b.group[0][0].toLowerCase()) ||
-                 compare(a.file.path, b.file.path) ||
-                 compare(a.context.line.start, b.context.line.start);
-
-        })
-        .forEach(function (item) {
-          sorted[type][item.context.name] = item;
-        });
-    });
-
-    return sorted;
-  },
-};
