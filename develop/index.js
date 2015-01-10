4eab30b388451ab4a46d106b446e9bbb03ad3dff
Valérian Galliat <val@codejam.info>
2015-01-10 23:22:42 +0100

    Update `develop` script to use new API

--- develop/index.js
+++ develop/index.js
@@ -1,2 +1,40 @@
-require('traceur-runner')({ asyncFunctions: true });
-require('./api');
+let path = require('path');
+let vfs = require('vinyl-fs');
+let through = require('through2');
+let sassdoc = require('../src/sassdoc');
+
+function inspect() {
+  let count = 0;
+
+  return through.obj((chunk, enc, cb) => {
+    count++;
+    cb(null, chunk);
+  }, (cb) => {
+    console.log(count);
+    cb();
+  });
+}
+
+function documentize() {
+  return sassdoc('./test/data', { verbose: true })
+    .then(() => {
+      console.log('yeah!');
+    });
+}
+
+function stream() {
+  return vfs.src('./test/data/**/*.scss')
+    .pipe(sassdoc({ verbose: true }))
+    .pipe(inspect())
+    .on('data', () => {});
+}
+
+(async function () {
+  try {
+    await documentize();
+    stream();
+  }
+  catch (err) {
+    console.error(err);
+  }
+}());
