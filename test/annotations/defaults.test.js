0806e843b68f5a5dd65e03dd594f86114efaae39
Pascal Duez <pascal@duez.me>
2015-01-27 20:19:07 +0100

    Add unit test for annotations with defaults - Refs #338

---
+++ test/annotations/defaults.test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+require('../init');
+
+var assert = require('assert');
+var File = require('vinyl');
+var sassdoc = require('../../');
+
+describe('#defaults', function () {
+  var dummy = {};
+
+  before(function () {
+    var file = new File({
+      path: 'test/fixture/dummy.scss',
+      contents: new Buffer('/// A dummy function\n@function dummy() {}'),
+    });
+
+    var stream = sassdoc.parseFilter();
+    stream.write(file);
+    stream.end();
+
+    return stream.promise.then(function (data) {
+      dummy = data[0];
+    });
+  });
+
+  it('should assign the proper default values', function () {
+    assert.deepEqual(['undefined'], dummy.group);
+    assert.strictEqual('public', dummy.access);
+  });
+
+});
