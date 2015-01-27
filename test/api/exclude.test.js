2d4a1fa96fcd4386ae2a9bfad3c929c4d4e43023
Pascal Duez <pascal@duez.me>
2015-01-27 16:04:08 +0100

    Add unit test for `exclude` configuration - Refs #338

---
+++ test/api/exclude.test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+require('../init');
+
+var assert = require('assert');
+var vfs = require('vinyl-fs');
+var through = require('through2');
+var exclude = require('../../dist/exclude');
+
+describe('#exclude', function () {
+  var files = [];
+
+  function inspect() {
+    return through.obj(function (file, enc, cb) {
+      files.push(file.relative);
+      cb(null, file);
+    });
+  }
+
+  before(function (done) {
+    vfs.src('./test/fixture/**/*.scss')
+      .pipe(exclude(['two.scss']))
+      .pipe(inspect())
+      .on('end', done)
+      .resume();
+  });
+
+  it('should properly exlude file patterns', function () {
+    assert.strictEqual(2, files.length);
+    assert.strictEqual(-1, files.indexOf('two.css'));
+  });
+
+});
