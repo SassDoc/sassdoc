4a5b44d6baf578a51fec2b0ec74180386ef83745
Fabrice Weinberg <Fabrice@weinberg.me>
2015-01-28 18:51:06 +0100

    Add some more tests for utils.is

--- test/utils/utils.test.js
+++ test/utils/utils.test.js
@@ -6,6 +6,7 @@ var assert = require('assert');
 var fs = require('fs');
 var utils = require('../../dist/utils');
 var readFile = utils.denodeify(fs.readFile);
+var errors = require('../../dist/errors');
 
 describe('#utils:denodeify', function () {
 
@@ -32,12 +33,36 @@ describe('#utils:denodeify', function () {
   });
 
   it('should provide utils.is.*', function () {
-
-    assert.ok(utils.is.stream({ pipe : function(){}}));
+    // .stream
+    assert.equal(utils.is.stream({ pipe : function(){}}), true);
     assert.equal(utils.is.stream(), undefined);
-
-    assert.ok(utils.is.undef());
-
+    // .undef
+    assert.equal(utils.is.undef(1), false);
+    assert.equal(utils.is.undef(), true);
+    // .error
+    assert.equal(utils.is.error(null), false);
+    assert.equal(utils.is.error(new errors.SassDocError()), true);
+    // .string
+    assert.equal(utils.is.string(), false);
+    // .function
+    assert.equal(utils.is['function'](), false);
+    assert.equal(utils.is['function'](function(){}), true);
+    // .object
+    assert.equal(utils.is.object(), false);
+    assert.equal(utils.is.object(1), false);
+    assert.equal(utils.is.object(''), false);
+    assert.equal(utils.is.object({}), true);
+    assert.equal(utils.is.object(new Error()), true);
+    // .plainObject
+    assert.equal(utils.is.plainObject(), false);
+    assert.equal(utils.is.plainObject(1), false);
+    assert.equal(utils.is.plainObject(new Error()), false);
+    assert.equal(utils.is.plainObject({}), true);
+    // .array
+    assert.equal(utils.is.array(), false);
+    assert.equal(utils.is.array(1), false);
+    assert.equal(utils.is.array(''), false);
+    assert.equal(utils.is.array([]), true);
   });
 
 });
