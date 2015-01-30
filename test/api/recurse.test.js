1ffb0d2f555bfec9399864ed4f2dcd4fabb3b367
Pascal Duez <pascal@duez.me>
2015-01-30 15:58:06 +0100

    Straighten `recurse` unit tests - Refs #338

--- test/api/recurse.test.js
+++ test/api/recurse.test.js
@@ -3,24 +3,22 @@
 require('../init');
 
 var assert = require('assert');
+var sinon = require('sinon');
 var vfs = require('vinyl-fs');
+var File = require('vinyl');
 var through = require('through2');
 var recurse = require('../../dist/recurse');
 
 describe('#recurse', function () {
   var files = [];
 
-  function inspect() {
-    return through.obj(function (file, enc, cb) {
-      files.push(file.relative);
-      cb(null, file);
-    });
-  }
-
   before(function (done) {
     vfs.src('./test/fixture/')
       .pipe(recurse())
-      .pipe(inspect())
+      .pipe(through.obj(function (file, enc, cb) {
+        files.push(file.relative);
+        cb(null, file);
+      }))
       .on('end', done)
       .resume();
   });
@@ -28,6 +26,56 @@ describe('#recurse', function () {
   it('should properly recurse a given directory', function () {
     assert.strictEqual(3, files.length);
     assert.deepEqual(files.sort(), [ 'one.scss', 'three.scss', 'two.scss' ]);
+  });
+
+});
+
+describe('#recurse-null', function () {
+  var files = [];
+  var nullFile = new File({
+    contents: null
+  });
+
+  before(function (done) {
+    vfs.src('./test/fixture/')
+      .pipe(through.obj(function (file, enc, cb) {
+        this.push(nullFile);
+        cb(null, file);
+      }))
+      .pipe(recurse())
+      .pipe(through.obj(function (file, enc, cb) {
+        files.push(file.relative);
+        cb(null, file);
+      }))
+      .on('end', done)
+      .resume();
+  });
+
+  it('should not pass null files through', function () {
+    assert.strictEqual(3, files.length);
+  });
+
+});
+
+describe('#recurse-fail', function () {
+  var spy = sinon.spy();
+
+  before(function (done) {
+    vfs.src('./test/fixture/')
+      .pipe(through.obj(function (file, enc, cb) {
+        this.push({ fail: 'fail' });
+        cb();
+      }))
+      .pipe(recurse())
+      .on('error', function () {
+        spy();
+        done();
+      })
+      .resume();
+  });
+
+  it('should fail if non Vinyl objects are passed', function () {
+    assert.ok(spy.called);
   });
 
 });
