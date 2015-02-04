295f270f69e4d2dc5c92910bd7607110d39a2fd9
Fabrice Weinberg <Fabrice@weinberg.me>
2015-02-04 22:18:27 +0100

    autofill access based on privatePrefix

--- src/annotation/annotations/access.js
+++ src/annotation/annotations/access.js
@@ -1,9 +1,17 @@
-export default function access() {
+export default function access(env) {
   return {
     name: 'access',
 
     parse(text) {
       return text.trim();
+    },
+
+    autofill(item) {
+      if (env.privatePrefix !== undefined) {
+        if ((new RegExp(env.privatePrefix)).test(item.context.name)) {
+          return 'private';
+        }
+      }
     },
 
     default() {
