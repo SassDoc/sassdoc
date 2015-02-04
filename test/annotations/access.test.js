e5e4cf526908873ed7cd8ca2a1525c086c43e7b9
Fabrice Weinberg <Fabrice@weinberg.me>
2015-02-04 22:18:27 +0100

    autofill access based on privatePrefix

--- test/annotations/access.test.js
+++ test/annotations/access.test.js
@@ -3,11 +3,19 @@
 var assert = require('assert');
 
 describe('#access', function () {
-  var access = (new (require('../../dist/annotation'))()).list.access;
+  var accessCtor = require('../../dist/annotation/annotations/access');
+  var access = accessCtor({});
+
   it('should return the trimmed string', function () {
     assert.equal(access.parse('   '), '');
     assert.equal(access.parse('   '), '');
     assert.equal(access.parse('\ntest\t'), 'test');
     assert.equal(access.parse('\nte\nst\t'), 'te\nst');
+  });
+
+  it('should autofill based on privatePrefix', function () {
+    var accessEnv = accessCtor({ privatePrefix: '^_' });
+    assert.equal(accessEnv.autofill({ context: { name: 'non-private'}, access: 'public'}), undefined);
+    assert.equal(accessEnv.autofill({ context: { name: '_private-name'}, access: 'public'}), 'private');
   });
 });
