0dda6506cc046f755e6161902ab07037c02da12f
Fabrice Weinberg <Fabrice@weinberg.me>
2015-02-04 23:30:14 +0100

    First draft

---
+++ src/annotation/annotations/name.js
@@ -0,0 +1,20 @@
+export default function access() {
+  return {
+    name: 'name',
+
+    parse(text) {
+      return text.trim();
+    },
+
+    // Abuse the autofill feature to rewrite the `item.context`
+    autofill(item){
+      if (item.name) {
+        item.context.name = item.name;
+        // Cleanup
+        delete item.name;
+      }
+    },
+
+    multiple: false
+  };
+}
---
+++ test/annotations/name.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var assert = require('assert');
+
+describe('#name', function () {
+  var name = (new (require('../../dist/annotation'))()).list.name;
+
+  it('should return the trimmed string', function () {
+    assert.equal(name.parse('   '), '');
+    assert.equal(name.parse('   '), '');
+    assert.equal(name.parse('\ntest\t'), 'test');
+    assert.equal(name.parse('\nte\nst\t'), 'te\nst');
+  });
+});
