bae60fa25201506e9983d12c45cba9f900d1f03f
Fabrice Weinberg <Fabrice@weinberg.me>
2015-02-04 22:46:50 +0100

    Add default privatePrefix

--- test/annotations/access.test.js
+++ test/annotations/access.test.js
@@ -13,9 +13,23 @@ describe('#access', function () {
     assert.equal(access.parse('\nte\nst\t'), 'te\nst');
   });
 
-  it('should autofill based on privatePrefix', function () {
-    var accessEnv = accessCtor({ privatePrefix: '^_' });
+  it('should autofill based on default', function () {
+    assert.equal(access.autofill({ context: { name: 'non-private'}, access: 'public'}), undefined);
+    assert.equal(access.autofill({ context: { name: '_private-name'}, access: 'public'}), 'private');
+    assert.equal(access.autofill({ context: { name: '-private-name'}, access: 'public'}), 'private');
+  });
+
+  it('should ignore autofill if privatePrefix is false', function () {
+    var accessEnv = accessCtor({ privatePrefix: false });
     assert.equal(accessEnv.autofill({ context: { name: 'non-private'}, access: 'public'}), undefined);
-    assert.equal(accessEnv.autofill({ context: { name: '_private-name'}, access: 'public'}), 'private');
+    assert.equal(accessEnv.autofill({ context: { name: '_private-name'}, access: 'public'}), undefined);
+    assert.equal(accessEnv.autofill({ context: { name: '-private-name'}, access: 'public'}), undefined);
+  });
+
+  it('should autofill based on privatePrefix', function () {
+    var accessEnv = accessCtor({ privatePrefix: '^--' });
+    assert.equal(accessEnv.autofill({ context: { name: '-non-private'}, access: 'public'}), undefined);
+    assert.equal(accessEnv.autofill({ context: { name: '_non-private'}, access: 'public'}), undefined);
+    assert.equal(accessEnv.autofill({ context: { name: '--private-name'}, access: 'public'}), 'private');
   });
 });
