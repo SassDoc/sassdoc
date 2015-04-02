6aa3e05e4a3388cc5b6e4c5fec92bd2c5bd4ee67
Pascal Duez <public@pascalduez.me>
2015-04-02 12:51:26 +0200

    Ensure `default` theme name is properly logged - Refs #391

--- src/environment.js
+++ src/environment.js
@@ -190,8 +190,6 @@ export default class Environment extends EventEmitter {
    * Process `this.theme`.
    */
   loadTheme() {
-    this.themeName = this.theme || 'default';
-
     if (this.theme === undefined) {
       return this.defaultTheme();
     }
@@ -201,6 +199,7 @@ export default class Environment extends EventEmitter {
     }
 
     let theme = this.resolve(this.theme, this.themeCwd);
+    this.themeName = this.theme;
     this.displayTheme = path.relative(process.cwd(), theme);
 
     return this.tryTheme(theme);
@@ -251,7 +250,7 @@ export default class Environment extends EventEmitter {
     }
 
     this.theme = require('sassdoc-theme-default');
-    this.themeName = 'default';
+    this.themeName = this.displayTheme = 'default';
   }
 
   /**
