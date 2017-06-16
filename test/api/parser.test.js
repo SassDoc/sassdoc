043d4d037c3c5e1eb521d9c0764ddd07b7bc12ae
Pascal Duez <public@pascalduez.me>
2017-06-16 10:46:11 +0200

    Remove extra semi-colon, fix linting

--- test/api/parser.test.js
+++ test/api/parser.test.js
@@ -5,7 +5,7 @@ var sinon = require('sinon')
 var mock = require('../mock')
 var Environment = require('../../dist/environment').default
 var Parser = require('../../dist/parser').default
-var source = require('vinyl-source-stream');
+var source = require('vinyl-source-stream')
 
 describe('#parser', function () {
   var warnings = []
