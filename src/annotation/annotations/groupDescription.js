2b3ec769dc4a1ba55d07038f657ebd1c0e519ddf
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-04-09 11:54:35 +0200

    add comment about "not-real" annotation

--- src/annotation/annotations/groupDescription.js
+++ src/annotation/annotations/groupDescription.js
@@ -1,3 +1,19 @@
+/**
+ * `@groupDescriptions` is should not be used on its own.
+ *
+ * It gets filled automatically from the lines following the `@group` annotation.
+ *
+ * @group example
+ * This is a group description. It describes the group.
+ * It can be split across multiple lines.
+ *
+ * {
+ *   'groupDescriptions': {
+ *     'example': 'This is a group description. It describes the group.\nIt can be split across multiple lines.'
+ *   }
+ * }
+ */
+
 export default function groupDescriptions () {
   return {
     name: 'groupDescriptions',
